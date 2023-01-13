e4c600c8d9e323be959262f253185934e09d98e6
EvelynMichell <119527374+EvelynMichell@users.noreply.github.com>
2023-01-13 14:41:08 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Mi practica con CODESandbox</h1>
+<div>
+  Universidad Tecnologica de Durango
+  <a href="https://github.com/EvelynMichell/contador.git" target="_blank" rel="noopener noreferrer">Mi GitHub</a>.
+</div>
+<h2>Evelyn Michell Gallegos Valdez</h2>
+`;
